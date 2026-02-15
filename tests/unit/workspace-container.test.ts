6d3cc05677209e63df80286f935fcccf1803415b
Christian Schulze <christian.schulze@gmail.com>
2026-02-15 16:14:36 +1100

    feat: add WorkspaceContainer with floating window tracking

---
+++ tests/unit/workspace-container.test.ts
@@ -0,0 +1,50 @@
+import { WorkspaceContainer } from "../../src/tree/workspace-container.ts";
+import { Container, ContainerType } from "../../src/tree/container.ts";
+
+describe("WorkspaceContainer", () => {
+  it("initializes workspace metadata", () => {
+    const workspace = new WorkspaceContainer(1, "1", 1, true);
+
+    expect(workspace.type).toBe(ContainerType.Workspace);
+    expect(workspace.name).toBe("1");
+    expect(workspace.number).toBe(1);
+    expect(workspace.visible).toBe(true);
+    expect(workspace.urgent).toBe(false);
+  });
+
+  it("tracks urgency", () => {
+    const workspace = new WorkspaceContainer(1, "1", 1, false);
+
+    workspace.urgent = true;
+
+    expect(workspace.urgent).toBe(true);
+  });
+
+  it("counts tiled windows recursively", () => {
+    const workspace = new WorkspaceContainer(1, "1", 1, true);
+    const split = new Container(2, ContainerType.Split);
+    const windowA = new Container(3, ContainerType.Window);
+    const windowB = new Container(4, ContainerType.Window);
+    const nestedSplit = new Container(5, ContainerType.Split);
+    const windowC = new Container(6, ContainerType.Window);
+
+    workspace.addChild(split);
+    split.addChild(windowA);
+    split.addChild(nestedSplit);
+    nestedSplit.addChild(windowB);
+    nestedSplit.addChild(windowC);
+
+    expect(workspace.tiledWindowCount()).toBe(3);
+  });
+
+  xit("tracks floating windows", () => {
+    const workspace = new WorkspaceContainer(1, "1", 1, true);
+    const floating = { windowId: 10 } as any;
+
+    workspace.addFloatingWindow(floating);
+    expect(workspace.getFloatingWindows()).toEqual([floating]);
+
+    workspace.removeFloatingWindow(floating);
+    expect(workspace.getFloatingWindows()).toEqual([]);
+  });
+});
